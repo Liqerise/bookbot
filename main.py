f491623e2852b0d7e66b24f99b5a40e76d27e3a1
Liqerise <lauren94f@gmail.com>
2024-03-25 19:48:47 +0100

    finished up the working bookbot

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
     #print (get_book_text())
-    print (get_word_count())
-    print (get_lettercount_dict())
+    #print (get_word_count())
+    #print (get_lettercount_dict())
+    sorted_chars = seperate_into_list(get_lettercount_dict())
+    sort(sorted_chars)
+    get_readable(sorted_chars)
+
+def get_input():
+    return 'books/frankenstein.txt'
 
 def get_book_text():
-    with open('books/frankenstein.txt') as text:
+    with open(get_input()) as text:
         file_contents = text.read()
         #print (file_contents)
         return (file_contents)
@@ -21,11 +27,33 @@ def get_lettercount_dict():
     lowered_text = text.lower()
     letter_count = {}
     for letter in lowered_text:
-        if letter not in letter_count:
-            letter_count[letter] = 0
-        letter_count[letter] += 1
+        if letter.isalpha():
+            if letter not in letter_count:
+                letter_count[letter] = 0
+            letter_count[letter] += 1
     return letter_count
-    
+
+def seperate_into_list(og_dict):
+    char_list = []
+    for char, freq in og_dict.items():
+        char_list.append({"char":char, "freq":freq})
+    return(char_list)
+
+def get_freq(dict):
+    return dict["freq"]
+
+def sort(item):
+    item.sort(key=get_freq, reverse=True)
+    return item
+
+def get_readable(meepmorp):
+    print (f"--- Begin report of {get_input()} ---")
+    print (f"{get_word_count()} words found in the document\n")
+    for item in meepmorp:
+        print (f"The '{item['char']}' character was found '{item['freq']}' times")
+    print ("--- End report ---")
+
+
 if __name__ == '__main__':
     main()
-  
+
